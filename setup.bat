58048cc38e7a6cd066690dba5256423ac53f26a8
Jonathan Chan <jchchan91@gmail.com>
2018-08-18 17:11:03 -0700

    Add PyBoy as submodule

--- setup.bat
+++ setup.bat
@@ -5,11 +5,11 @@ pushd %scriptDir%
 
 set externalDir=%scriptDir%\external
 if not exist %externalDir% (
-    mkdir %externalDir
+    mkdir %externalDir%
 )
 
 REM Clone pyboy
-set pyboyDir=%externalDir%\pyboyDir
-git submodule add https://github.com/Baekalfen/PyBoy.git %pyboyDir%
+cd %externalDir%
+git submodule add https://github.com/Baekalfen/PyBoy.git
 
 popd
