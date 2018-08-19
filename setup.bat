8ef63d9daee337448fba9e03605985d048268aa3
Jonathan Chan <jchchan91@gmail.com>
2018-08-18 17:13:39 -0700

    Fix setup scripts

--- setup.bat
+++ setup.bat
@@ -3,13 +3,6 @@
 set scriptDir=%~dp0
 pushd %scriptDir%
 
-set externalDir=%scriptDir%\external
-if not exist %externalDir% (
-    mkdir %externalDir%
-)
 
-REM Clone pyboy
-cd %externalDir%
-git submodule add https://github.com/Baekalfen/PyBoy.git
 
 popd
